3c29d929d3fa54735975f4fc57d873ca178e819a
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-07-15 13:38:47 +0530

    add foreign key

--- hksnote/models.py
+++ hksnote/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class mynt(models.Model):
+    tid = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     Title=models.CharField(max_length=150)
     Tag=models.CharField(max_length=500)
     Note=models.TextField()
